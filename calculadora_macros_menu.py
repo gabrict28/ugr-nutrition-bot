11f28539ae648958c401be6667de61611a14c7f4
gabrict28 <gabrict28@gmail.com>
2026-03-03 13:04:02 +0100

    Añadido script para calcular y visualizar los macros totales del menú

---
+++ calculadora_macros_menu.py
@@ -0,0 +1,46 @@
+import json
+
+INPUT_NUTRI_MENU="info_nutricional_menu.json"
+
+def calcular_macros():
+    with open(INPUT_NUTRI_MENU, 'r', encoding='utf-8') as menu:
+        info = json.load(menu)
+
+        for sede, dias in info.items():
+            print(f"SEDE: {sede.upper()}")
+            
+            for fecha, platos in dias.items():
+                print(f"{fecha}")
+                total_cal = 0
+                total_prot = 0
+                total_fat = 0
+                total_carb = 0
+
+                #Imprimimos cada plato
+                for p in platos:
+                    nombre = p.get('nombre', 'Plato')
+                    nutri = p.get('nutricion')
+
+                    if nutri:
+                        p_kcal = nutri.get('kcal', 0)
+                        p_prot = nutri.get('proteinas', 0)
+                        p_fat = nutri.get('grasas', 0)
+                        p_carbs = nutri.get('carbohidratos', 0)
+                        print(f"🍴 {nombre[:30]:<30} | {p_kcal:>5} kcal | P: {p_prot:>4}g | CH: {p_carbs:>4}g | G: {p_fat:>4}g")
+
+                        #Acumulamos para el resumen
+                        total_cal += p_kcal
+                        total_prot += p_prot
+                        total_fat += p_fat
+                        total_carb += p_carbs
+                    else:
+                        print(f"No hay información nutricional al respecto (comunicar error)")
+                
+                #Resumen
+                print(f"{'-'*40}")
+                print(f"TOTAL DÍA: {round(total_cal, 1)} kcal")
+                print(f"Macros: Prot: {round(total_prot, 1)}g | Carbs: {round(total_carb, 1)}g | Grasas: {round(total_fat, 1)}g")
+                print(f"{'-'*40}")
+
+if __name__ == "__main__":
+    calcular_macros()
